1269bea4eaca74c20dada0d730443efeebf63550
Md Mahbub Alam Khan <hkhan.cse@gmail.com>
2025-04-21 12:25:17 +0300

    ❌ Removed: Auto updater file.

--- includes/autoupdater/updater.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$updaterBase = 'https://projects.bluewindlab.net/wpplugin/zipped/plugins/';
-$pluginRemoteUpdater = $updaterBase . 'baf/notifier_ftfwc.php';
-new WpAutoUpdater(FAQTFW_ADDON_CURRENT_VERSION, $pluginRemoteUpdater, FAQTFW_ADDON_UPDATER_SLUG);
