76a21a6e2c2a279bdc2f2ae7d73b13d84a986e77
Md Mahbub Alam Khan <hkhan.cse@gmail.com>
2025-04-22 05:17:29 +0300

    🔥 Updated: Plugin options input field tooltip.

--- includes/Callbacks/OptionsPanel/Fields/AllFieldsCb.php
+++ includes/Callbacks/OptionsPanel/Fields/AllFieldsCb.php
@@ -59,177 +59,85 @@ class AllFieldsCb {
 		$value      = $this->options[ $field_id ] ?? 100; // change default value.
 		$hints      = esc_html__( 'Set number like- 1,2,3. Set big number(100, 200, 300) to display FAQ tab at the last of tab contain.', 'baf-faqtfw' );
 
-		echo $this->get_text_field( $field_name, $field_id, intval($value), '', 'small-text', $hints ); //phpcs:ignore
-
+		echo $this->get_text_field( $field_name, $field_id, ( intval($value) === 0 ) ? 100: $value, '', 'small-text', $hints ); //phpcs:ignore
 	}
 
-
+	/**
+	 * Tab counter field.
+	 */
 	public function faqftw_faq_counter_settings() {
 
-		$faqftw_options = get_option( 'faqftw_options' );
+		$field_id   = 'faqftw_faq_counter'; // change the id.
+		$field_name = $this->options_id . "[{$field_id}]";
+		$value      = $this->options[ $field_id ] ?? 1; // change default value.
+		$hints      = esc_html__( 'Show total number of FAQ items.', 'baf-faqtfw' );
 
-		$faqftw_faq_counter = 1;
-
-		if ( isset( $faqftw_options['faqftw_faq_counter'] ) ) {
-
-			$faqftw_faq_counter = $faqftw_options['faqftw_faq_counter'];
-		}
-
-		if ( $faqftw_faq_counter == 1 ) {
-
-			$show_status = 'selected=selected';
-			$hide_status = '';
-		} else {
-
-			$show_status = '';
-			$hide_status = 'selected=selected';
-		}
-
-		echo '<select name="faqftw_options[faqftw_faq_counter]">
-									<option value="1" ' . $show_status . '>' . esc_html__( 'Show', 'baf-faqtfw' ) . '</option>   
-									<option value="0" ' . $hide_status . '>' . esc_html__( 'Hide', 'baf-faqtfw' ) . '</option>               
-							 </select><em><small> ' . esc_html__( 'Show total number of FAQ items.', 'baf-faqtfw' ) . '</small></em>';
+		echo $this->get_select_field( $field_name, $field_id, $this->get_boolean_dropdown_options(), $value, $hints  ); //phpcs:ignore
 	}
 
 
+	/**
+	 * Tab auto hide field.
+	 */
 	public function faqftw_auto_hide_tab_settings() {
 
-		$faqftw_options = get_option( 'faqftw_options' );
+		$field_id   = 'faqftw_auto_hide_tab'; // change the id.
+		$field_name = $this->options_id . "[{$field_id}]";
+		$value      = $this->options[ $field_id ] ?? 0; // change default value.
 
-		$faqftw_auto_hide_tab = 1;
-
-		if ( isset( $faqftw_options['faqftw_auto_hide_tab'] ) ) {
-
-			$faqftw_auto_hide_tab = $faqftw_options['faqftw_auto_hide_tab'];
-		}
-
-		if ( $faqftw_auto_hide_tab == 1 ) {
-
-			$show_status = 'selected=selected';
-			$hide_status = '';
-		} else {
-
-			$show_status = '';
-			$hide_status = 'selected=selected';
-		}
-
-		echo '<select name="faqftw_options[faqftw_auto_hide_tab]">
-									<option value="1" ' . $show_status . '>' . esc_html__( 'Yes', 'baf-faqtfw' ) . '</option>   
-									<option value="0" ' . $hide_status . '>' . esc_html__( 'No', 'baf-faqtfw' ) . '</option>               
-							 </select>';
+		echo $this->get_select_field( $field_name, $field_id, $this->get_boolean_dropdown_options(), $value  ); //phpcs:ignore
 	}
 
+	/**
+	 * Tab search box field.
+	 */
 	public function faqftw_show_search_box_settings() {
 
-		$faqftw_options = get_option( 'faqftw_options' );
+		$field_id   = 'faqftw_show_search_box'; // change the id.
+		$field_name = $this->options_id . "[{$field_id}]";
+		$value      = $this->options[ $field_id ] ?? 1; // change default value.
 
-		$faqftw_show_search_box = 1;
+		echo $this->get_select_field( $field_name, $field_id, $this->get_boolean_dropdown_options(), $value  ); //phpcs:ignore
 
-		if ( isset( $faqftw_options['faqftw_show_search_box'] ) ) {
-
-			$faqftw_show_search_box = $faqftw_options['faqftw_show_search_box'];
-		}
-
-		if ( $faqftw_show_search_box == 1 ) {
-
-			$show_status = 'selected=selected';
-			$hide_status = '';
-		} else {
-
-			$show_status = '';
-			$hide_status = 'selected=selected';
-		}
-
-		echo '<select name="faqftw_options[faqftw_show_search_box]">
-									<option value="1" ' . $show_status . '>' . esc_html__( 'Yes', 'baf-faqtfw' ) . '</option>   
-									<option value="0" ' . $hide_status . '>' . esc_html__( 'No', 'baf-faqtfw' ) . '</option>               
-							 </select>';
 	}
 
-
+	/**
+	 * Tab voting box field.
+	 */
 	public function faqftw_show_voting_box_settings() {
 
-		$faqftw_options = get_option( 'faqftw_options' );
+		$field_id   = 'faqftw_show_voting_box'; // change the id.
+		$field_name = $this->options_id . "[{$field_id}]";
+		$value      = $this->options[ $field_id ] ?? 1; // change default value.
 
-		$faqftw_show_voting_box = 1;
-
-		if ( isset( $faqftw_options['faqftw_show_voting_box'] ) ) {
-
-			$faqftw_show_voting_box = $faqftw_options['faqftw_show_voting_box'];
-		}
-
-		if ( $faqftw_show_voting_box == 1 ) {
-
-			$show_status = 'selected=selected';
-			$hide_status = '';
-		} else {
-
-			$show_status = '';
-			$hide_status = 'selected=selected';
-		}
-
-		echo '<select name="faqftw_options[faqftw_show_voting_box]">
-									<option value="1" ' . $show_status . '>' . esc_html__( 'Yes', 'baf-faqtfw' ) . '</option>   
-									<option value="0" ' . $hide_status . '>' . esc_html__( 'No', 'baf-faqtfw' ) . '</option>               
-							 </select>';
+		echo $this->get_select_field( $field_name, $field_id, $this->get_boolean_dropdown_options(), $value  ); //phpcs:ignore
 	}
 
-
+	/**
+	 * Tab meta box field.
+	 */
 	public function faqftw_show_meta_box_settings() {
 
-		$faqftw_options = get_option( 'faqftw_options' );
+		$field_id   = 'faqftw_show_meta_box'; // change the id.
+		$field_name = $this->options_id . "[{$field_id}]";
+		$value      = $this->options[ $field_id ] ?? 1; // change default value.
 
-		$faqftw_show_meta_box = 1;
+		echo $this->get_select_field( $field_name, $field_id, $this->get_boolean_dropdown_options(), $value  ); //phpcs:ignore
 
-		if ( isset( $faqftw_options['faqftw_show_meta_box'] ) ) {
-
-			$faqftw_show_meta_box = $faqftw_options['faqftw_show_meta_box'];
-		}
-
-		if ( $faqftw_show_meta_box == 1 ) {
-
-			$show_status = 'selected=selected';
-			$hide_status = '';
-		} else {
-
-			$show_status = '';
-			$hide_status = 'selected=selected';
-		}
-
-		echo '<select name="faqftw_options[faqftw_show_meta_box]">
-									<option value="1" ' . $show_status . '>' . esc_html__( 'Yes', 'baf-faqtfw' ) . '</option>   
-									<option value="0" ' . $hide_status . '>' . esc_html__( 'No', 'baf-faqtfw' ) . '</option>               
-							 </select>';
 	}
 
-
+	/**
+	 * Tab pagination field.
+	 */
 	public function faqftw_enable_pagination_settings() {
 
-		$faqftw_options = get_option( 'faqftw_options' );
+		$field_id   = 'faqftw_enable_pagination'; // change the id.
+		$field_name = $this->options_id . "[{$field_id}]";
+		$value      = $this->options[ $field_id ] ?? 1; // change default value.
 
-		$faqftw_enable_pagination = 1;
+		echo $this->get_select_field( $field_name, $field_id, $this->get_boolean_dropdown_options(), $value  ); //phpcs:ignore
 
-		if ( isset( $faqftw_options['faqftw_enable_pagination'] ) ) {
-
-			$faqftw_enable_pagination = $faqftw_options['faqftw_enable_pagination'];
-		}
-
-		if ( $faqftw_enable_pagination == 1 ) {
-
-			$show_status = 'selected=selected';
-			$hide_status = '';
-		} else {
-
-			$show_status = '';
-			$hide_status = 'selected=selected';
-		}
-
-		echo '<select name="faqftw_options[faqftw_enable_pagination]">
-									<option value="1" ' . $show_status . '>' . esc_html__( 'Yes', 'baf-faqtfw' ) . '</option>   
-									<option value="0" ' . $hide_status . '>' . esc_html__( 'No', 'baf-faqtfw' ) . '</option>               
-							 </select>';
 	}
-
 
 	/**
 	 * FAQ item per page field.
@@ -240,7 +148,7 @@ class AllFieldsCb {
 		$field_name = $this->options_id . "[{$field_id}]";
 		$value      = $this->options[ $field_id ] ?? 5; // change default value.
 
-		echo $this->get_text_field( $field_name, $field_id, intval($value), '', 'small-text' ); //phpcs:ignore
+		echo $this->get_text_field( $field_name, $field_id, ( intval($value) === 0 ) ? 5: $value  , '', 'small-text' ); //phpcs:ignore
 
 	}
 }
--- includes/Traits/OptionsFieldsTraits.php
+++ includes/Traits/OptionsFieldsTraits.php
@@ -11,6 +11,7 @@ trait OptionsFieldsTraits {
 	 * @param bool   $value The value of the field.
 	 * @param string $placeholder Placeholder text.
 	 * @param string $class_name The class of the field.
+	 * @param string $hints Hints for the field.
 	 *
 	 * @return string
 	 */
@@ -22,7 +23,80 @@ trait OptionsFieldsTraits {
 			esc_attr( $class_name ),
 			esc_attr( $value ),
 			esc_attr( $placeholder ),
-			( ! empty( $hints ) ) ? '<em><small>' . esc_html( $hints ) . '</small></em>' : ''
+			( ! empty( $hints ) ) ? $this->get_field_hints( $hints ) : ''
 		);
+	}
+
+	/**
+	 * Select field
+	 *
+	 * @param string     $name  The name of the field.
+	 * @param string     $id    The id of the field.
+	 * @param array      $options Default options for the field.
+	 * @param string|int $selected The value of the field.
+	 * @param string     $hints Hints for the field.
+	 *
+	 * @return string
+	 */
+	public function get_select_field( $name, $id, $options = [], $selected = '', $hints = '' ) {
+
+		$options_html = '';
+
+		foreach ( $options as $value => $label ) {
+			$options_html .= sprintf(
+                "<option value='%s'%s>%s</option>",
+                esc_attr( $value ),
+                selected( $selected, $value, false ),
+                esc_html( $label )
+			);
+		}
+
+		return sprintf(
+			"<select name='%s' id='%s'>%s</select>%s",
+			esc_attr( $name ),
+			esc_attr( $id ),
+			$options_html,
+			( ! empty( $hints ) ) ? $this->get_field_hints( $hints ) : ''
+		);
+	}
+
+
+	/**
+	 * Get field hints
+	 *
+	 * This function returns the hints for a field.
+     *
+	 * @param string $hints The hints for the field.
+	 *
+	 * @return string
+	 */
+	private function get_field_hints( $hints ) {
+		return sprintf(
+			'<span class="bwl-opt-tooltip"><i class="dashicons dashicons-info"></i><span class="tooltip-text">%s</span></span>',
+			esc_html( $hints )
+		);
+	}
+
+	/**
+	 * Get boolean dropdown options
+	 *
+	 * This function returns the options for a boolean dropdown field.
+     *
+	 * @param array $data The data for the field.
+	 *
+	 * @return array
+	 */
+	public function get_boolean_dropdown_options( $data = [] ) {
+
+		$options = [
+			'1' => esc_html__( 'Yes', 'baf-faqtfw' ),
+			'0' => esc_html__( 'No', 'baf-faqtfw' ),
+		];
+
+		if ( ! empty( $data ) ) {
+			$options = array_merge( $options, $data );
+		}
+
+		return $options;
 	}
 }
